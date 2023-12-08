c7747f14f245b8884a5ba0101dd3c8f6a472ab2b
Emma Peterson <emmacooperpeterson@gmail.com>
2023-12-08 11:29:48 -0600

    ENH day 7 part 2

--- 2023/day_7/solution.py
+++ 2023/day_7/solution.py
@@ -3,6 +3,30 @@ import numpy as np
 import pandas as pd
 import re
 
+PART = 2  # 1 or 2
+
+
+def adjust_j(card_groups):
+    """
+    This is for part 2. If there are any Js in the card_groups, append them to
+    the largest non-J group.
+
+    e.g. [["A", "A"], [2], [3], ["J"]] --> [["A", "A", "J"], [2], [3]]
+    """
+
+    num_j = sum([item=="J" for sublist in card_groups for item in sublist])
+    if num_j == 0 or num_j == 5:
+        return card_groups
+
+    else:
+        j_group = ["J"] * num_j
+        non_j_groups = [g for g in card_groups if "J" not in g]
+        non_j_groups.sort(key=len)
+        non_j_groups[-1] += j_group
+        return non_j_groups
+
+
+# setup
 with open("input.txt", "r") as f:
     hands = f.read().splitlines()
 
@@ -14,13 +38,17 @@ cards = hands["cards"].apply(lambda x: pd.Series(list(x)))
 cards.columns = [f"card_{i}" for i in range(1, 6)]
 hands = pd.concat([cards, hands], axis=1)
 
-# create card lists
-# e.g. "233KT" --> [[2], [3, 3], [K], [T]]
+# create card groups, e.g. "233KT" --> [[2], [3, 3], [K], [T]]
 hands["cards"] = hands.apply(lambda row: ''.join(sorted(row["cards"])), axis=1)
 
 hands["card_groups"] = hands.apply(
     lambda row: [list(g) for k, g in groupby(row["cards"])], axis=1
 )
+
+if PART == 2:  # append Js to largest non-J group
+    hands["card_groups"] = hands.apply(
+        lambda row: adjust_j(row["card_groups"]), axis=1
+    )
 
 # for each hand, count number of unique cards and size of largest group
 hands["n_groups"] = hands.apply(lambda row: len(row["card_groups"]), axis=1)
@@ -44,7 +72,12 @@ conditions = [
 hands["hand_rank"] = np.select(conditions, hand_ranks)
 
 # convert all cards to numbers
-hands = hands.replace({"T": 10, "J": 11, "Q": 12, "K": 13, "A": 14})
+replacements = {
+    1: {"T": 10, "J": 11, "Q": 12, "K": 13, "A": 14},
+    2: {"T": 10, "J": 1, "Q": 12, "K": 13, "A": 14}
+}
+
+hands = hands.replace(replacements[PART])
 hands = hands.astype({f"card_{i}": "int" for i in range(1, 6)})
 
 # sort and rank
